f1ed87d3336063a00304ab1fdaa94ef50fa161d4
Anton Bryhagen <anton.bryhagen@gmail.com>
2024-09-21 10:13:38 +0200

    Initial Commit

---
+++ upLink 2/upLink_2App.swift
@@ -0,0 +1,17 @@
+//
+//  upLink_2App.swift
+//  upLink 2
+//
+//  Created by Anton Bryhagen on 2024-09-21.
+//
+
+import SwiftUI
+
+@main
+struct upLink_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
