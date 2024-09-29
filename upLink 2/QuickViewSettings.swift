64ac588b1b0ee388a3a7ba3dba6c4cc7dd997050
Anton Bryhagen <anton.bryhagen@gmail.com>
2024-09-30 00:18:35 +0200

    Add placeholder text to settings page

--- upLink 2/QuickViewSettings.swift
+++ upLink 2/QuickViewSettings.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct QuickViewSettings: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Select sensors to display on home screen")
+        }
     }
 }
 
